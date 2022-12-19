c73edcd96c7f37f0178b4d32b9e4ee019bf32a17
Nicholas T Dimidik <nicholas.dimidik@sjsu.edu>
2022-12-18 19:24:37 -0800

    Added tailwind css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,17 @@
-import './globals.css'
-
+'use client';
+import '../styles/dist.css'
+import React, { useState } from 'react';
+import Image from 'next/image';
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const [show, setShow] = useState<Boolean>(false);
+  const [profile, setProfile] = useState(false);
+  const [product, setProduct] = useState(false);
+  const [deliverables, setDeliverables] = useState(false);
+
   return (
     <html lang="en">
       {/*
@@ -12,7 +19,12 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body className="h-full w-full">
+                
+        <main className=''>
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
