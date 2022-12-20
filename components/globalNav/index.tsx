acd35625f98663c8c2add59875221185ccb045d5
Nicholas T Dimidik <nicholas.dimidik@sjsu.edu>
2022-12-19 22:19:39 -0800

    1

--- components/globalNav/index.tsx
+++ components/globalNav/index.tsx
@@ -2,16 +2,19 @@ import Image from "next/image";
 import logo from '../../public/favicon.ico'
 export default function GlobalNavBar () {
     return (
-        <div className="bg-neutral-800 h-14 border-b border-b-neutral-300 flex justify-between">
+        <div className="bg-neutral-800 h-14 border-b border-b-neutral-600 overflow-hidden relative">
+            <div className="flex justify-between center">
             
-            <Image src={logo} width={48} height={48} alt="logo"/>
-            
-            <ul className="flex gap-3 text-white">
-                <li>Home</li>
-                <li>Search</li>
-            </ul>
+                {/* Image to go here */}
+                <p className="bg-blue-600 rounded-full h-10 w-10"></p>            
+                <ul className="flex gap-3 text-white">
+                    
+                    <li>Home</li>
+                    <li>Search</li>
+                </ul>
 
-             <div className="text-white">Account</div>
+                <div className="text-white">Account</div>
+            </div>
         </div>
     );
 }
