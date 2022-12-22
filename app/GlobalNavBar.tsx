bed9a55f2b5b93327350c6cbe212a8e389ac56a2
Nicholas T Dimidik <nicholas.dimidik@sjsu.edu>
2022-12-22 15:28:24 -0800

    Inline-block background

--- app/GlobalNavBar.tsx
+++ app/GlobalNavBar.tsx
@@ -1,8 +1,7 @@
 import UnderOnSelectBtn from "../components/buttons/UnderOnSelectBtn";
 export default function GlobalNavBar () {
     return (
-        <div className="bg-neutral-800 h-28 md:h-14 border-b border-b-neutral-600 p-1">
-            <div className="h-14 md:hidden float-left"></div>
+        <div className="bg-neutral-800 h-28 md:h-14 border-b border-b-neutral-600 inline-block align-middle w-full">
             <div className="bg-slate-800 grid md:grid-cols-3 md:grid-rows-1 gap-3 md:gap-0 items-center grid-cols-2 grid-rows-2">
             
                 {/* Image to go here */}
@@ -11,7 +10,7 @@ export default function GlobalNavBar () {
                     
                 </div>   
 
-                <ul className="flex justify-center gap-3 text-white col-span-2 md:col-span-1 order-3 md:order-2 bg-red-300 p-1">
+                <ul className="flex justify-center gap-3 text-white col-span-2 md:col-span-1 order-3 md:order-2 bg-red-300 h-full">
                     <li>
                         <UnderOnSelectBtn>1</UnderOnSelectBtn>
                     </li>
@@ -24,7 +23,7 @@ export default function GlobalNavBar () {
                     
                 </ul>
 
-                <div className="flex justify-end text-white bg-purple-400 order-2 md:order-3">Account</div>
+                <div className="flex justify-end text-white order-2 md:order-3">Account</div>
             </div>
         </div>
     );
