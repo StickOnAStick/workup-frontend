8a20061f5f79338477d4802a78bc99d1bb6a8259
Nicholas T Dimidik <nicholas.dimidik@sjsu.edu>
2022-12-29 13:10:18 -0800

    Add / Update: Added Icons to navbar UnderOnSelectBtn now has active state

--- app/GlobalNavBar.tsx
+++ app/GlobalNavBar.tsx
@@ -1,7 +1,6 @@
-import Link from "next/link";
-import UnderOnSelectBtn from "../components/buttons/UnderOnSelectBtn";
-import { FiHome } from 'react-icons/fi';
+import NavBarIcons from "./NavBarIcons";
 export default function GlobalNavBar () {
+   
     return (
         <div className="bg-neutral-800 h-28 md:h-14 border-b border-b-neutral-600 inline-block align-middle w-full">
             <div className="py-1 px-3 grid grid-cols-2 grid-rows-2 md:grid-cols-3 md:grid-rows-1 gap-3 md:gap-0 items-center ">
@@ -11,29 +10,7 @@ export default function GlobalNavBar () {
                     <p className=" bg-blue-600 rounded-full h-10 w-10"></p>  
                 </div>   
 
-                <ul className="flex justify-center gap-3 text-white col-span-2 md:col-span-1 order-3 md:order-2 h-full">
-                    <li>
-                        <UnderOnSelectBtn>
-                            <div>Buisness</div>
-                        </UnderOnSelectBtn>
-                    </li>
-                    <li>
-                        <Link href="/">
-                            <UnderOnSelectBtn>
-                                <FiHome/>
-                                
-                            </UnderOnSelectBtn>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/search">
-                            <UnderOnSelectBtn>
-                                Search
-                            </UnderOnSelectBtn>
-                        </Link>
-                    </li>
-                    
-                </ul>
+                <NavBarIcons/>
 
                 <div className="flex justify-end text-white order-2 md:order-3">Account</div>
             </div>
--- components/buttons/UnderOnSelectBtn.tsx
+++ components/buttons/UnderOnSelectBtn.tsx
@@ -4,21 +4,21 @@
 
 export default function UnderOnSelectBtn ({
     children,
+    active
 }:{
     children: React.ReactNode
-    
+    active: boolean
 }) {
-    
 
-    
 
     return (
-    <div className=" hover:bg-neutral-700 w-20 h-12 xs:w-28 sm:h-12 rounded-lg flex justify-center items-center hover:cursor-pointer font-bold tracking-wide ">
-        <span className=" border-b border-b-blue-700 ">
+    
+    <div className="relative hover:bg-neutral-700 w-20 h-12 xs:w-28 sm:h-12 rounded-lg flex justify-center items-center hover:cursor-pointer font-bold tracking-wide ">
+        
             <div>
             {children}
             </div>
-        </span>
+            <span className={`${active ? ' absolute h-[3px] w-full bottom-0 md:bottom-[-4px] bg-blue-500 rounded-md': ''}`}/>
     </div>
     );
 
